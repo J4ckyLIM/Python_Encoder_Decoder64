d8b823ff7665ecdeecedf616480cc0e74cbff501
JackyLIM <jacky.lim@edu.itescia.fr>
2019-12-10 11:04:59 +0100

    Function2: Convert character to ascii code

---
+++ src/function2.py
@@ -0,0 +1,18 @@
+"""
+Second function
+"""
+
+
+def convert_char_to_ascii(converted_list) -> list:
+    # This function takes the list as parameter
+    # Then create a list
+    # Then converts the list of character to ascii
+    # Then returns the new list
+    char_converted_to_ascii = []
+    for letter in converted_list:
+        char_converted_to_ascii.append(ord(letter))
+    print(char_converted_to_ascii)
+    return char_converted_to_ascii
+
+
+convert_char_to_ascii(["A", "B"])
