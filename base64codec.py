54109380501c61fe7b166a92252a8ca5cecb3155
JackyLIM <jacky.lim@edu.itescia.fr>
2019-12-10 13:00:52 +0100

    Function7: Converting int to base64 value

--- base64codec.py
+++ base64codec.py
@@ -5,16 +5,7 @@ import sys
 """Python decoder/encoder 64
 
 """
-"""
-Dictionary base64code
-"""
-n2ch = "".join([
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "abcdefghijklmnopqrstuvwxyz",
-    "0123456789-_",
-])
-ch2n = dict(zip(n2ch, range(len(n2ch))))
-print(n2ch)
+
 
 
 
