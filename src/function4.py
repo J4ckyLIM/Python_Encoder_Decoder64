f1b627b54c1c0837cca581658229e7ec34e9087c
Unknown <leo.faucher@edu.itescia.fr>
2019-12-10 11:44:57 +0100

    added print for testing

--- src/function4.py
+++ src/function4.py
@@ -7,4 +7,5 @@ def eight_bit_list_to_string(eight_bit_list):
     # This function takes the list as parameter
     # Then creates a string concatenating the elements
     eight_bit_string = ""
+    print(eight_bit_string.join(eight_bit_list))
     return eight_bit_string.join(eight_bit_list)
